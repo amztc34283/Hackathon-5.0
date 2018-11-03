1234c59daa7faa8ac302fb3efa4b5d6a4951d64e
Brandon Hsieh <hsiehbrandon@berkeley.edu>
2018-11-03 16:00:10 -0700

    parse fb msgs

---
+++ parse_msg.py
@@ -0,0 +1,36 @@
+import json
+import sys
+import time
+
+
+
+# Parses a message from a facebook messenger json file, extracts the content
+# given by the person with name. Returns a string of the person's conversation.
+def parse_msg(file_name, name):
+    with open(file_name) as data_file:
+        msg_dict = json.load(data_file)
+        msg_lst = msg_dict["messages"]
+        content_lst = [msg["content"].encode('ascii', 'ignore') for msg in msg_lst if                      msg['sender_name'] == name]
+        content_str = " ".join(content_lst)
+        return content_str
+
+
+def make_json(content_str):
+    data = {}
+    data["content_items"] = []
+    data["content_items"].append({
+        "content":content_str,
+        "content_type": "text/plain",
+        "created" :str(time.time()) })
+    with open("watson_in.json", "w") as out_file:
+        json.dump(data, out_file)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        "Correct usage is python clean_msg.py <message.json> <Person_Name>"
+    else:
+        file_name = sys.argv[1]
+        name = sys.argv[2]
+        content_str = parse_msg(file_name, name)
+        make_json(content_str)
