bcebe93a567ec335e79369e983da6d5ab060f58d
BillyChau <billy.chau.vital@gmail.com>
2018-11-03 15:08:18 -0700

    added some test files and python api example

---
+++ test.py
@@ -0,0 +1,18 @@
+from watson_developer_cloud import PersonalityInsightsV3
+import json
+
+service = PersonalityInsightsV3(
+     version='2017-10-13',
+## url is optional, and defaults to the URL below. Use the correct URL for your region.
+     url='https://gateway-wdc.watsonplatform.net/personality-insights/api',
+     iam_apikey='W7wjiNAzqzpVxaXhYMFMQ138rNY6g7GrwXVYj5nHzU7l')
+
+with open('test.json') as \
+        profile_json:
+    profile = service.profile(
+        profile_json.read(),
+        content_type='application/json',
+        raw_scores=True,
+        consumption_preferences=True).get_result()
+
+print(json.dumps(profile, indent=2))
